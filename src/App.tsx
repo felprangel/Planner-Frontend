d7dd1acb67c02ca9e3557d7623b402f6e09c01e7
felprangel <feliperangel897@gmail.com>
2024-07-12 22:06:59 -0300

    adiciona key

--- src/App.tsx
+++ src/App.tsx
@@ -135,12 +135,15 @@ export function App() {
 
 						<div className="flex flex-wrap gap-2">
 							{emailsToInvite.map((email) => (
-								<div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
-								<span className="text-zinc-300">{email}</span>
-								<button type="button">
-									<X className="size-4 text-zinc-400" />
-								</button>
-							</div>
+								<div
+									key={email}
+									className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2"
+								>
+									<span className="text-zinc-300">{email}</span>
+									<button type="button">
+										<X className="size-4 text-zinc-400" />
+									</button>
+								</div>
 							))}
 						</div>
 
