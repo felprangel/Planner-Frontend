78c21b1c1e575fc360c327f238eddf82524d5bf0
felprangel <feliperangel897@gmail.com>
2024-07-12 21:46:44 -0300

    indentação

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,11 @@ export function App() {
 
 					{isGuestsInputOpen && (
 						<div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-							<button onClick={openGuestsModal} type="button" className="flex items-center gap-2 flex-1 text-left">
+							<button
+								onClick={openGuestsModal}
+								type="button"
+								className="flex items-center gap-2 flex-1 text-left"
+							>
 								<UserRoundPlus className="size-5 text-zinc-400" />
 								<span className="text-zinc-400 text-lg flex-1">
 									Quem estará na viagem?
