00f74be9d8f9b8b2286b2d71f050e3b806a69fff
felprangel <feliperangel897@gmail.com>
2024-07-13 16:54:29 -0300

    adiciona layout da main

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, MapPin, Plus, Settings2 } from 'lucide-react'
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from 'lucide-react'
 
 export function TripDetailsPage() {
 	return (
@@ -23,7 +23,7 @@ export function TripDetailsPage() {
 				</div>
 			</div>
 
-			<main className="flex gap-16">
+			<main className="flex gap-16 px-4">
 				<div className="flex-1 space-y-6">
 					<div className="flex items-center justify-between">
 						<h2 className="text-3xl font-semibold">Atividades</h2>
@@ -43,6 +43,29 @@ export function TripDetailsPage() {
 							<p className="text-zinc-500 text-sm">
 								Nenhuma atividade cadastrada nessa data.
 							</p>
+						</div>
+
+						<div className="space-y-2.5">
+							<div className="flex gap-2 items-baseline">
+								<span className="text-xl text-zinc-300 font-semibold">
+									Dia 18
+								</span>
+								<span className="text-xs text-zinc-500">Domingo</span>
+							</div>
+							<div className="space-y-2.5">
+								<div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+									<CircleCheck className="size-5 text-lime-300" />
+									<span className="text-zinc-100">Academia em grupo</span>
+									<span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+								</div>
+							</div>
+                            <div className="space-y-2.5">
+								<div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+									<CircleCheck className="size-5 text-lime-300" />
+									<span className="text-zinc-100">Academia em grupo</span>
+									<span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
