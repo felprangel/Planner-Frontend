54805a33380b75826d6a1a8889e80852e8bd1fc9
felprangel <feliperangel897@gmail.com>
2024-07-13 17:08:17 -0300

    adiciona container de conviados

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,10 +1,12 @@
 import {
 	Calendar,
 	CircleCheck,
+	CircleDashed,
 	Link2,
 	MapPin,
 	Plus,
 	Settings2,
+	UserCog,
 } from 'lucide-react'
 
 export function TripDetailsPage() {
@@ -115,7 +117,39 @@ export function TripDetailsPage() {
 						</button>
 					</div>
 					<div className="w-full h-px bg-zinc-800" />
-					<div></div>
+					<div>
+						<div className="space-y-6">
+							<h2 className="font-semibold text-xl">Convidados</h2>
+							<div className="space-y-5">
+								<div className="flex items-center justify-between gap-4">
+									<div className="space-y-1.5">
+										<span className="block font-medium text-zinc-100">
+											John Doe
+										</span>
+										<span className="block text-sm text-zinc-400 truncate">
+											johndoe@acme.com
+										</span>
+									</div>
+									<CircleDashed className="text-zinc-400 size-5 shrink-0" />
+								</div>
+								<div className="flex items-center justify-between gap-4">
+									<div className="space-y-1.5">
+										<span className="block font-medium text-zinc-100">
+											Rafael Policarpo
+										</span>
+										<span className="block text-sm text-zinc-400 truncate">
+											costinha@gmail.com
+										</span>
+									</div>
+									<CircleDashed className="text-zinc-400 size-5 shrink-0" />
+								</div>
+							</div>
+							<button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center justify-center gap-2 hover:bg-zinc-700 w-full">
+								<UserCog className="size-5" />
+								Gerenciar convidados
+							</button>
+						</div>
+					</div>
 				</div>
 			</main>
 		</div>
