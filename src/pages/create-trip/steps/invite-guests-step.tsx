859fdf64a0b4b80423aec8cfee0c8d349882a47d
felprangel <feliperangel897@gmail.com>
2024-07-13 16:32:46 -0300

    adiciona componente de envio de convidados

---
+++ src/pages/create-trip/steps/invite-guests-step.tsx
@@ -0,0 +1,39 @@
+import { ArrowRight, UserRoundPlus } from "lucide-react";
+
+interface InviteGuestsStepProps {
+    openGuestsModal: () => void
+    openConfirmTripModal: () => void
+    emailsToInvite: string[]
+}
+
+export function InviteGuestStep(props: InviteGuestsStepProps) {
+	return (
+		<div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+			<button
+				onClick={props.openGuestsModal}
+				type="button"
+				className="flex items-center gap-2 flex-1 text-left"
+			>
+				<UserRoundPlus className="size-5 text-zinc-400" />
+				{props.emailsToInvite.length === 0 ? (
+					<span className="text-zinc-100 text-lg flex-1">
+						{props.emailsToInvite.length} pessoa(s) convidada(s)
+					</span>
+				) : (
+					<span className="text-zinc-400 text-lg flex-1">
+						Quem estará na viagem?
+					</span>
+				)}
+			</button>
+
+			<div className="w-px h-6 bg-zinc-800" />
+
+			<button
+				onClick={props.openConfirmTripModal}
+				className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+			>
+				Confirmar viagem <ArrowRight className="size-5" />
+			</button>
+		</div>
+	)
+}
