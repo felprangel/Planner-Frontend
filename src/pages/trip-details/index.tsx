2a0bbd3546cfa0fb4158c44e6c8d1eb25089a3a1
felprangel <feliperangel897@gmail.com>
2024-07-13 17:05:10 -0300

    adiciona parte de links

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,11 @@
-import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from 'lucide-react'
+import {
+	Calendar,
+	CircleCheck,
+	Link2,
+	MapPin,
+	Plus,
+	Settings2,
+} from 'lucide-react'
 
 export function TripDetailsPage() {
 	return (
@@ -59,7 +66,7 @@ export function TripDetailsPage() {
 									<span className="text-zinc-400 text-sm ml-auto">08:00h</span>
 								</div>
 							</div>
-                            <div className="space-y-2.5">
+							<div className="space-y-2.5">
 								<div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
 									<CircleCheck className="size-5 text-lime-300" />
 									<span className="text-zinc-100">Academia em grupo</span>
@@ -69,7 +76,47 @@ export function TripDetailsPage() {
 						</div>
 					</div>
 				</div>
-				<div className="w-80"></div>
+				<div className="w-80 space-y-6">
+					<div className="space-y-6">
+						<h2 className="font-semibold text-xl">Links Importantes</h2>
+						<div className="space-y-5">
+							<div className="flex items-center justify-between gap-4">
+								<div className="space-y-1.5">
+									<span className="block font-medium text-zinc-100">
+										Reserva do Air Bnb
+									</span>
+									<a
+										href="#"
+										className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+									>
+										www.airbnb.com.br/rooms/118781268055206370542342334235234353452334
+									</a>
+								</div>
+								<Link2 className="text-zinc-400 size-5 shrink-0" />
+							</div>
+							<div className="flex items-center justify-between gap-4">
+								<div className="space-y-1.5">
+									<span className="block font-medium text-zinc-100">
+										Reserva do Air Bnb
+									</span>
+									<a
+										href="#"
+										className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+									>
+										www.airbnb.com.br/rooms/118781268055206370542342334235234353452334
+									</a>
+								</div>
+								<Link2 className="text-zinc-400 size-5 shrink-0" />
+							</div>
+						</div>
+						<button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center justify-center gap-2 hover:bg-zinc-700 w-full">
+							<Plus className="size-5" />
+							Cadastrar novo link
+						</button>
+					</div>
+					<div className="w-full h-px bg-zinc-800" />
+					<div></div>
+				</div>
 			</main>
 		</div>
 	)
