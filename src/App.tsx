787d4a535334475464a47b44c1a327647e93859e
felprangel <feliperangel897@gmail.com>
2024-07-12 21:47:02 -0300

    adiciona modal de convidados

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {
 	ArrowRight,
 	UserRoundPlus,
 	Settings2,
+	X,
 } from 'lucide-react'
 import { useState } from 'react'
 
@@ -108,6 +109,19 @@ export function App() {
 					</a>
 				</p>
 			</div>
+
+			{isGuestsModalOpen && (
+				<div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+					<div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900">
+						<div className="flex items-center justify-between">
+							<h2>Selecionar convidados</h2>
+							<button type="button">
+								<X className="size-5 text-zinc-400" />
+							</button>
+						</div>
+					</div>
+				</div>
+			)}
 		</div>
 	)
 }
