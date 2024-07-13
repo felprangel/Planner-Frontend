4c9ceb426285908718a8a4ff2abe8b5bc11d70f0
felprangel <feliperangel897@gmail.com>
2024-07-12 22:36:31 -0300

    altera texto do botão

--- src/App.tsx
+++ src/App.tsx
@@ -257,7 +257,7 @@ export function App() {
 							type="submit"
 							className="bg-lime-300 text-lime-950 w-full justify-center rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400"
 						>
-							Convidar <Plus className="size-5" />
+							Confirmar criação da viagem
 						</button>
 					</form>
 				</div>
