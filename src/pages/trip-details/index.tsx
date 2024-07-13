2f02c5f993d878abb020272a38e243f4dc418c8d
felprangel <feliperangel897@gmail.com>
2024-07-13 16:45:31 -0300

    adiciona header da main

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, MapPin, Settings2 } from 'lucide-react'
+import { Calendar, MapPin, Plus, Settings2 } from 'lucide-react'
 
 export function TripDetailsPage() {
 	return (
@@ -22,6 +22,18 @@ export function TripDetailsPage() {
 					</button>
 				</div>
 			</div>
+
+			<main className="flex gap-16">
+				<div className="flex-1">
+					<div className="flex items-center justify-between">
+						<h2 className="text-3xl font-semibold">Atividades</h2>
+						<button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+							<Plus className="size-5" /> Cadastrar atividade
+						</button>
+					</div>
+				</div>
+				<div className="w-80"></div>
+			</main>
 		</div>
 	)
 }
