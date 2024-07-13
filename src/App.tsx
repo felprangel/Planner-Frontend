2d2f6718c74e50826e39604e7a527cf6afceaf4e
felprangel <feliperangel897@gmail.com>
2024-07-12 22:46:14 -0300

    adiciona rota para as páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { CreateTripPage } from './pages/create-trip'
+import { TripDetailsPage } from './pages/trip-details'
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <div>Hello world!</div>,
+		element: <CreateTripPage />,
+	},
+	{
+		path: '/trips/:tripId',
+		element: <TripDetailsPage />,
 	},
 ])
 
