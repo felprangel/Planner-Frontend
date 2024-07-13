b4236afdd2deb837a4a6a4de39910542dacba32c
felprangel <feliperangel897@gmail.com>
2024-07-13 16:41:08 -0300

    diminui texto

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -6,13 +6,13 @@ export function TripDetailsPage() {
 			<div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
 				<div className="flex items-center gap-2">
 					<MapPin className="size-5 text-zinc-400" />
-					<span className="text-lg text-zinc-100">Florianópolis, Brasil</span>
+					<span className="text-zinc-100">Florianópolis, Brasil</span>
 				</div>
 
 				<div className="flex items-center gap-5">
 					<div className="flex items-center gap-2">
 						<Calendar className="size-5 text-zinc-400" />
-						<span className="text-lg text-zinc-100">17 a 23 de Agosto</span>
+						<span className="text-zinc-100">17 a 23 de Agosto</span>
 					</div>
 
 					<div className="w-px h-6 bg-zinc-800" />
