73cfd7d750472c21122ddd334819d9ad5e05e667
felprangel <feliperangel897@gmail.com>
2024-07-13 16:44:53 -0300

    adiciona espaçamento

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -2,7 +2,7 @@ import { Calendar, MapPin, Settings2 } from 'lucide-react'
 
 export function TripDetailsPage() {
 	return (
-		<div className="max-w-6xl px-6 py-10 mx-auto">
+		<div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
 			<div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
 				<div className="flex items-center gap-2">
 					<MapPin className="size-5 text-zinc-400" />
