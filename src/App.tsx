956cb3b472b552c7c8de1f18c8af651f55f499e9
felprangel <feliperangel897@gmail.com>
2024-07-12 22:38:03 -0300

    indentação

--- src/App.tsx
+++ src/App.tsx
@@ -129,7 +129,10 @@ export function App() {
 
 							<div className="w-px h-6 bg-zinc-800" />
 
-							<button onClick={openConfirmTripModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+							<button
+								onClick={openConfirmTripModal}
+								className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+							>
 								Confirmar viagem <ArrowRight className="size-5" />
 							</button>
 						</div>
