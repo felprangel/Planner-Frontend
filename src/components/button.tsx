2846130b1586f2d177bdb6c5f00cf981c0a124f9
felprangel <feliperangel897@gmail.com>
2024-07-14 10:45:04 -0300

    adiciona estilização variante no botão

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,25 @@
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+import { tv, VariantProps } from 'tailwind-variants'
+
+const buttonVariants = tv({
+    base: 'rounded-lg px-5 py-2 font-medium flex items-center gap-2',
+    variants: {
+        variant: {
+            primary: 'bg-lime-300 text-lime-950 hover:bg-lime-400',
+            secondary: 'bg-zinc-800 text-zinc-200 hover:bg-zinc-700',
+        }
+    },
+    defaultVariants: {
+        variant: 'primary'
+    }
+})
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
     children: React.ReactNode
 }
 
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({ children, variant, ...props }: ButtonProps) {
 	return (
-		<button {...props} className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+		<button {...props} className={buttonVariants({ variant })}>
 			{children}
 		</button>
 	)
