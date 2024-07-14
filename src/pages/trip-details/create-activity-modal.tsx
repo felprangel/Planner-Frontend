658d4204d7eefd9d5eaec269a687b1f8fb00ea1f
felprangel <feliperangel897@gmail.com>
2024-07-14 10:48:58 -0300

    utiliza componente botão

--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,4 +1,5 @@
 import { Calendar, Tag, X } from 'lucide-react'
+import { Button } from '../../components/button'
 
 interface CreateActivityModalProps {
 	closeCreateActivityModal: () => void
@@ -45,12 +46,9 @@ export function CreateActivityModal(props: CreateActivityModalProps) {
 						</div>
 					</div>
 
-					<button
-						type="submit"
-						className="bg-lime-300 text-lime-950 w-full justify-center rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400"
-					>
+					<Button variant="primary" size="full">
 						Salvar atividade
-					</button>
+					</Button>
 				</form>
 			</div>
 		</div>
