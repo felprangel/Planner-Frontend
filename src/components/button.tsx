e24b49ee1ba6a45df8fe5b40bbfa9e13f42b8fb0
felprangel <feliperangel897@gmail.com>
2024-07-14 10:35:41 -0300

    adiciona propriedades do botao

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,10 +2,10 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode
 }
 
-export function Button(props: ButtonProps) {
+export function Button({ children, ...props }: ButtonProps) {
 	return (
-		<button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
-			{props.children}
+		<button {...props} className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+			{children}
 		</button>
 	)
 }
