cd988e9f2a56e92838ffe695d8a50be36cd67769
felprangel <feliperangel897@gmail.com>
2024-07-14 10:25:22 -0300

    adiciona componente de convidados

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,15 +1,14 @@
 import {
 	Calendar,
 	CircleCheck,
-	CircleDashed,
 	MapPin,
 	Plus,
 	Settings2,
-	UserCog,
 } from 'lucide-react'
 import { useState } from 'react'
 import { CreateActivityModal } from './create-activity-modal'
 import { ImportantLinks } from './important-links'
+import { Guests } from './guests'
 
 export function TripDetailsPage() {
 	const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
@@ -97,44 +96,14 @@ export function TripDetailsPage() {
 				<div className="w-80 space-y-6">
 					<ImportantLinks />
 					<div className="w-full h-px bg-zinc-800" />
-					<div>
-						<div className="space-y-6">
-							<h2 className="font-semibold text-xl">Convidados</h2>
-							<div className="space-y-5">
-								<div className="flex items-center justify-between gap-4">
-									<div className="space-y-1.5">
-										<span className="block font-medium text-zinc-100">
-											John Doe
-										</span>
-										<span className="block text-sm text-zinc-400 truncate">
-											johndoe@acme.com
-										</span>
-									</div>
-									<CircleDashed className="text-zinc-400 size-5 shrink-0" />
-								</div>
-								<div className="flex items-center justify-between gap-4">
-									<div className="space-y-1.5">
-										<span className="block font-medium text-zinc-100">
-											Rafael Policarpo
-										</span>
-										<span className="block text-sm text-zinc-400 truncate">
-											costinha@gmail.com
-										</span>
-									</div>
-									<CircleDashed className="text-zinc-400 size-5 shrink-0" />
-								</div>
-							</div>
-							<button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center justify-center gap-2 hover:bg-zinc-700 w-full">
-								<UserCog className="size-5" />
-								Gerenciar convidados
-							</button>
-						</div>
-					</div>
+					<Guests />
 				</div>
 			</main>
 
 			{isCreateActivityModalOpen && (
-				<CreateActivityModal closeCreateActivityModal={closeCreateActivityModal} />
+				<CreateActivityModal
+					closeCreateActivityModal={closeCreateActivityModal}
+				/>
 			)}
 		</div>
 	)
