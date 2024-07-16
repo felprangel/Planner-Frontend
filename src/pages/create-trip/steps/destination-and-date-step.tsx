2d1a854a61d33c327aba4192213b39bc73e53296
felprangel <feliperangel897@gmail.com>
2024-07-15 22:10:54 -0300

    adiciona modal para calendário

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,5 +1,6 @@
-import { ArrowRight, Calendar, MapPin, Settings2 } from 'lucide-react'
+import { ArrowRight, Calendar, MapPin, Settings2, X } from 'lucide-react'
 import { Button } from '../../../components/button'
+import { useState } from 'react'
 
 interface DestinationAndDateStepProps {
 	isGuestsInputOpen: boolean
@@ -8,6 +9,16 @@ interface DestinationAndDateStepProps {
 }
 
 export function DestinationAndDateStep(props: DestinationAndDateStepProps) {
+	const [isDatePickerOpen, setIsDatePickerOpen] = useState(false)
+
+	function openDatePicker() {
+		setIsDatePickerOpen(true)
+	}
+
+	function closeDatePicker() {
+		setIsDatePickerOpen(false)
+	}
+
 	return (
 		<div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
 			<div className="flex items-center gap-2 flex-1">
@@ -21,13 +32,29 @@ export function DestinationAndDateStep(props: DestinationAndDateStepProps) {
 			</div>
 			<button
 				disabled={props.isGuestsInputOpen}
+				onClick={openDatePicker}
 				className="flex items-center gap-2 text-left"
 			>
 				<Calendar className="size-5 text-zinc-400" />
-				<span className="text-lg text-zinc-400 w-40">
-					Quando?
-				</span>
+				<span className="text-lg text-zinc-400 w-40">Quando?</span>
 			</button>
+
+			{isDatePickerOpen && (
+				<div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+					<div className="w-[320px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+						<div className="space-y-2">
+							<div className="flex items-center justify-between">
+								<h2 className="text-lg font-semibold">Selecione a data</h2>
+								<button onClick={closeDatePicker} type="button">
+									<X className="size-5 text-zinc-400" />
+								</button>
+							</div>
+						</div>
+
+						<h1>Hello world</h1>
+					</div>
+				</div>
+			)}
 
 			<div className="w-px h-6 bg-zinc-800" />
 
